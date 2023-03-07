cd95a589e92dbe18d3f7a8d4b0b948d2c54f7ca0
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-07 08:40:09 +0000

    register api working

---
+++ db/config.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://shekharonlineproject:shekharonlineproject@cluster0.8rwhj8f.mongodb.net/e-comm?retryWrites=true&w=majority');
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+require('./db/config');
+const User = require('./db/User');
 const app = express();
 
-const connectDb = async () => {
-  mongoose.connect('mongodb+srv://shekharonlineproject:shekharonlineproject@cluster0.8rwhj8f.mongodb.net/e-comm?retryWrites=true&w=majority'
-  )
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model('products', productSchema);
-  const data = await product.find();
-  console.log('data', data);
-}
+// const connectDb = async () => {
+ 
+//   const productSchema = new mongoose.Schema({});
+//   const product = mongoose.model('products', productSchema);
+//   const data = await product.find();
+//   console.log('data', data);
+// }
 
-connectDb();
+// connectDb();
 
 app.get('/', (req, resp) => {
   resp.send("get Api is working...")
 })
 
+app.post('/register', (req, resp)=> {
+  resp.send('register api working...');
+})
 
-app.listen(5000);
+app.listen(5001);
